7b81932d80e050314ad225f8827f60ee7b86d712
yig319 <yig319@users.noreply.github.com>
2024-09-27 20:43:57 +0000

    deploy: 1d82c00154dee2d94a17b7bf0ba2a914ebe5750e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+ga6d7a2a',
+    VERSION: '0.0.post1.dev1+g1d82c00',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
